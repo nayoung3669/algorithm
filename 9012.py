afb5d585fc943c8d2daa77be2c6c8d5470585828
Nayoung <skdud3669@gmail.com>
2023-05-24 17:56:41 +0900

    9012 init

---
+++ 9012.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for i in range(t):
+    stack = list(input())
+    open = "("
+    close = ")"
+    if stack[0] == close:
+        print("NO")
+        break
+    for j in stack:
+        pass
