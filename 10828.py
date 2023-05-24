70ef1c39feff0e12db2b6aae0065f40a12f954a6
Nayoung <skdud3669@gmail.com>
2023-05-24 14:13:39 +0900

    solution

---
+++ 10828.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+stack = []
+for i in range(n):
+    command=input().split()
+
+    if command[0] == 'push':
+        stack.append(command[1])
+
+    elif command[0] == 'pop':
+        if len(stack):
+            print(stack.pop())
+        else:
+            print(-1)
+
+    elif command[0] == 'top':
+        if len(stack):
+            print(stack[-1])
+        else:
+            print(-1)
+
+    elif command[0] == 'size':
+        print(len(stack))
+    
+    elif command[0] == 'empty':
+        if len(stack):
+            print(0)
+        else:
+            print(-1)
+    
